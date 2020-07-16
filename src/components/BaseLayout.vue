3a07415880d47a88e80cfd8016753482a23bd5f5
kyuseokyi <kyuseok.y@gmail.com>
2020-07-16 15:51:45 +0900

    add vuex, bootstrap, pwa support

---
+++ src/components/BaseLayout.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-md-3 d-none d-lg-block d-xl-none d-xl-block">
+                <slot name="left"></slot>
+            </div>
+            <div class="col">
+                <div class="d-block d-sm-none d-none d-sm-block d-md-block d-lg-none">
+                    <slot name="section-dropdown"></slot>
+                </div>
+                <slot name="right"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "BaseLayout"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './registerServiceWorker'
+import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app')
